5383084329c9e7236fe95bcd6b5f0b88be0bc6ba
mohra-55 <mohra2612@gmail.com>
2026-07-08 23:47:23 +0300

    auth

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,75 @@
 package org.example;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.net.URL;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws  InterruptedException {
+        // valid senario
+        WebDriver driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        driver.navigate().to("https://bstackdemo.com/signin");
+
+
+
+        WebElement userName = driver.findElement(By.id("username"));
+        userName.click();
+        WebElement userNameText = driver.findElement(By.xpath("//*[text()='demouser']"));
+        Thread.sleep(2000);
+        userNameText.click();
+
+
+        WebElement pass = driver.findElement(By.id("password"));
+        pass.click();
+        WebElement passText = driver.findElement(By.xpath("//*[text()='testingisfun99']"));
+        Thread.sleep(2000);
+        passText.click();
+
+
+        WebElement loginButton = driver.findElement(By.id("login-btn"));
+        loginButton.click();
+
+        Thread.sleep(5000);
+        String currentUrl = driver.getCurrentUrl();
+
+        if(currentUrl.contains("signin=true")){
+            System.out.println("page open successfully");
         }
+        else {
+            System.out.println("page didn't open");
+        }
+
+        WebElement logoutButton = driver.findElement(By.id("signin"));
+        logoutButton.click();
+        Thread.sleep(1000);
+        logoutButton.click();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
